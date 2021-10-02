186cd272ac941fd5e94a7708770cd9b538e69d52
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-02 16:01:09 +0200

    Add and style placeholder on Input component

--- src/posts/components/form/forms/firstForm/FirstForm.js
+++ src/posts/components/form/forms/firstForm/FirstForm.js
@@ -4,25 +4,28 @@ import Button from "../../../UI/button/Button";
 function FirstForm({ title, author, address, updateTitle, updateAuthor, updateAddress, submit }) {
   return (
     <form onSubmit={submit}>
-      <Input 
+      <Input
         type="text" 
         value={title} 
         onChange={updateTitle}
         name="title" 
+        placeholder="My first post"
         isRequired 
       />
       <Input 
         type="text" 
         value={author}
         onChange={updateAuthor}
-        name="author" 
+        name="author"
+        placeholder="John Smith"
         isRequired 
       />
       <Input 
         type="text" 
         value={address}
         onChange={updateAddress}
-        name="address" 
+        name="address"
+        placeholder="john.smith@matrix.com"
         isRequired 
       />
       <Button type="submit">
--- src/posts/components/form/input/Input.js
+++ src/posts/components/form/input/Input.js
@@ -1,15 +1,16 @@
 import classes from "./Input.module.css";
 
-function TextInput({ name, type, value, onChange, isRequired }) {
+function TextInput({ name, type, value, onChange, placeholder, isRequired }) {
   return (
     <div className={classes.root}>
       <label className={classes.label} htmlFor={name}>{name}</label>
-      <input 
+      <input
         className={classes.input}
         type={type}
         id={name} 
         name={name}
         value={value}
+        placeholder={placeholder}
         onChange={onChange} 
         required={isRequired} 
       />
