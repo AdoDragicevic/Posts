4f0ba9ab93845a9b7e813d1ffd4b47e9c6691cea
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-02 20:54:34 +0200

    Customize search btn and list item

--- src/posts/components/list/List.js
+++ src/posts/components/list/List.js
@@ -1,14 +1,11 @@
 import ListItem from "./ListItem";
-import Card from "../layout/card/Card";
 
 import classes from "./List.module.css";
 
 function List({ posts, show }) {
   if (posts.length === 0) return <p>No posts...</p>;
-  const items = posts.map( p => (
-    <Card>
+  const items = posts.map( p => ( 
       <ListItem key={p.id} {...p} show={show} />
-    </Card>
   ));
   return <ul className={classes.root}> {items} </ul>;
 };
--- src/posts/components/list/ListItem.js
+++ src/posts/components/list/ListItem.js
@@ -3,10 +3,10 @@ import classes from "./ListItem.module.css";
 function ListItem({ img, title, address, author, id, show }) {
   return (
     <li className={classes.root} onClick={ () => show(id) }>
-      <img src={img} />
       <h4>{title}</h4>
       <h6>{author}</h6>
       <p>{address}</p>
+      <div className={classes.line} />
     </li>
   )
 };
