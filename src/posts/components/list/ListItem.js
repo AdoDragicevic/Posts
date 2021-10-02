871db89c0f8de2b2919c64cb7db7fddab80c0a80
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-02 20:17:14 +0200

    Add minor styling improvements

--- src/posts/components/list/ListItem.js
+++ src/posts/components/list/ListItem.js
@@ -1,10 +1,11 @@
 import classes from "./ListItem.module.css";
 
-function ListItem({ img, title, address, id, show }) {
+function ListItem({ img, title, address, author, id, show }) {
   return (
     <li className={classes.root} onClick={ () => show(id) }>
       <img src={img} />
       <h4>{title}</h4>
+      <h6>{author}</h6>
       <p>{address}</p>
     </li>
   )
