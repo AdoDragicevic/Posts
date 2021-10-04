cffa56c4838d86631e5c863e24ba80a108c9e2a4
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-04 09:37:24 +0200

    Style list item

--- src/posts/components/list/ListItem.js
+++ src/posts/components/list/ListItem.js
@@ -6,11 +6,12 @@ function ListItem({ img, title, address, author, id, openPost }) {
   return (
     <li className={classes.root} onClick={ () => openPost(id) }>
       <Header>{title}</Header>
-      <br />
-      <img className={classes.img} src={img} />
+      <div className={classes.imgBox}>
+        <img className={classes.img} src={img} />
+      </div>
       <div className={classes.info}>
-        <p className={classes.author}>{author}</p>
         <p className={classes.address}>{address}</p>
+        <p className={classes.author}>{author}</p>
       </div>
       <div className={classes.line} />
     </li>
