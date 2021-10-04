c98a64e19ba2b100a0e558bcbf091905b477c38a
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-04 11:15:45 +0200

    Update seedData and add loremIpsum import

---
+++ src/loremIpsum.js
@@ -0,0 +1,9 @@
+const loremIpsum = ` Lorem ipsum,
+      
+  dolor sit, amet consectetur adipiscing elit netus habitantras pharetra hendrerit platea faucibus, fames lobortis odio arcu interdum natoque nascetur eleifend fusce taciti ad augue. 
+
+  Mi faucibus dapibus rutrum habitasse hendrerit sociis, vitae aenean phasellus in vel vivamus pretium, nec a mauris lacus netus. Sociosqu quam felis accumsan justo potenti laoreet rhoncus enim, nisi pellentesque parturient suscipit odio facilisis netus tristique, phasellus natoque habitant elementum varius litora mauris nam. 
+
+  Erat facilisi turpis venenatis "ornare ridiculus quis".`;
+
+export default loremIpsum;
--- src/seedData.js
+++ src/seedData.js
@@ -1,21 +1,32 @@
+import loremIpsum from "./loremIpsum"
+
 const seedData = [
   {
     title: "Why I'm the Big Boss",
     author: "Solid snake",
     address: "Metal Gear Solid",
-    description: "Clone of Big Boss",
-    img: "",
-    id: "1",
+    description: loremIpsum,
+    img: "https://res.cloudinary.com/dggbkwj5l/image/upload/v1633337820/adodragi/wzxotrrjyaxamf6km6gs.jpg",
+    id: "06e7ca4-00a6-68e-d6b6-cac2e388b8a",
     date: "22.09.2021"
   },
   {
     title: "What when you have all 7 dragon balls",
     author: "Vegeta",
     address: "Dragon Ball Z",
-    description: "Bad ass m*** f***",
-    img: "",
-    id: "2",
+    description: loremIpsum,
+    img: "https://res.cloudinary.com/dggbkwj5l/image/upload/v1633337861/adodragi/gkddzeusfupdvqnn5whi.png",
+    id: "3b6fcbc-73c5-818-6573-efc6c48cbb15",
     date: "01.10.2021."
+  },
+  {
+    title: "Why we are doomed to repeat the mistakes of our forefathers",
+    author: "Hal Emmerich, AKA. Otacon",
+    address: "otacon@rex.com",
+    description: loremIpsum,
+    img: "https://res.cloudinary.com/dggbkwj5l/image/upload/v1633337767/adodragi/p25iebecg1at6wkrgbjx.jpg",
+    id: "3b6fcbc-73c5-818-6573-efc6c48cbb15",
+    date: "02.10.2021."
   }
 ];
 
