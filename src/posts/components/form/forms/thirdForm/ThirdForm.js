98d4e46c1f72f7a03c6b39653aaa47160700ca3f
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-04 10:17:40 +0200

    Style Third form and Textarea components

--- src/posts/components/form/forms/thirdForm/ThirdForm.js
+++ src/posts/components/form/forms/thirdForm/ThirdForm.js
@@ -1,20 +1,25 @@
 import Textarea from "../../textarea/Textarea";
 import Button from "../../../UI/button/Button";
 
+import classes from "./ThirdForm.module.css";
+
+
 function ThirdForm({ description, updateDescription, goBack, submit }) {
 
   return (
     <form onSubmit={submit}>
-      <Textarea 
+      <Textarea
         name="description"
         value={description}
         onChange={updateDescription}
         isRequired 
       />
-      <div onClick={goBack}>&#8592;</div>
-      <Button type="submit" size="large" color="secondary">
-        Submit
-      </Button>
+      <div className={classes.footer}>
+        <div className={classes.goBack} onClick={goBack}>&#8592;</div>
+        <Button type="submit" size="large" color="secondary">
+          Submit
+        </Button>
+      </div>
     </form>
   )
 };
