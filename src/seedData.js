5f5f53cd3d7d6d1f0b9939a5064af85cdb9f93d8
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-04 11:24:13 +0200

    Minor change to seed data

--- src/seedData.js
+++ src/seedData.js
@@ -8,7 +8,7 @@ const seedData = [
     description: loremIpsum,
     img: "https://res.cloudinary.com/dggbkwj5l/image/upload/v1633337820/adodragi/wzxotrrjyaxamf6km6gs.jpg",
     id: "06e7ca4-00a6-68e-d6b6-cac2e388b8a",
-    date: "22.09.2021"
+    date: "4.10.2021"
   },
   {
     title: "What when you have all 7 dragon balls",
@@ -26,7 +26,7 @@ const seedData = [
     description: loremIpsum,
     img: "https://res.cloudinary.com/dggbkwj5l/image/upload/v1633337767/adodragi/p25iebecg1at6wkrgbjx.jpg",
     id: "3b6fcbc-73c5-818-6573-efc6c48cbb15",
-    date: "02.10.2021."
+    date: "1.10.2021."
   }
 ];
 
