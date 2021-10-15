4c7e0c6fe27c6c8881e68df0f9835af75fd8e9c6
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-16 00:42:32 +0200

    Make style changes to improve design on small devices like Iphone SE

--- src/components/UI/form/formContent/textarea/Textarea.js
+++ src/components/UI/form/formContent/textarea/Textarea.js
@@ -3,9 +3,7 @@ import classes from "./Textarea.module.css";
 function Textarea({ name, value, onChange }) {
   return (
     <div className={classes.root}>
-      <label className={classes.label} htmlFor={name}>
-        {name}
-      </label>
+      <label className={classes.label} htmlFor={name}> {name} </label>
       <textarea 
         className={classes.textarea}
         id={name} 
--- src/components/pages/index/search/searchInput/SearchInput.js
+++ src/components/pages/index/search/searchInput/SearchInput.js
@@ -2,14 +2,13 @@ import classes from "./SearchInput.module.css";
 
 function SearchInput({ criteria, switchCriteria, nOfResults, onChange }) {
 
-  const css = `${classes.root} ${nOfResults === 0 ? classes.red : classes.green}`;
-  const btnTxt = `Searching by ${criteria}`;
+  const css = `${classes.input} ${nOfResults === 0 ? classes.red : classes.green}`;
 
   return (
-    <div className={css}>
-      <button className={classes.btn} onClick={switchCriteria}> {btnTxt} </button>
-      <input className={classes.input} type="text" placeholder="Search..." onChange={onChange} />
-    </div>
+    <>
+      <button className={classes.btn} onClick={switchCriteria}> {`By ${criteria}`} </button>
+      <input className={css} type="text" placeholder="Search..." onChange={onChange} />
+    </>
   )
 };
 
