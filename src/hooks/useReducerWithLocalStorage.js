1a7a19cfcaaab8b3941aa251325653649645d877
Ado Dragicevic <adodragicevic@gmail.com>
2021-10-20 08:14:49 +0200

    Add style changes for mobile devices, remove one warning

--- src/hooks/useReducerWithLocalStorage.js
+++ src/hooks/useReducerWithLocalStorage.js
@@ -4,7 +4,7 @@ function useReducerWithLocalStorage(reducer, defaultState, key) {
   const [state, dispatch] = useReducer(reducer, defaultState, () => (
     JSON.parse(window.localStorage.getItem(key)) || defaultState
   ));
-  useEffect( () => window.localStorage.setItem(key, JSON.stringify(state)), [state] );
+  useEffect( () => window.localStorage.setItem(key, JSON.stringify(state)), [state, key] );
   return [state, dispatch];
 };
 
